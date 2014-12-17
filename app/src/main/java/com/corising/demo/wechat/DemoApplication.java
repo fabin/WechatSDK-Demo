0238bc51cb23c3b0530350f3154d3ac1c36054d5
闫发宾 <fabin@yanfabins-MacBook-Pro.local>
2014-12-17 17:55:20 +0800

    Mack it multiDex To avoid DEX’s 64k limit. See section <i>Using the official solution</i> in https://medium.com/@mustafa01ali/dexs-64k-limit-is-not-a-problem-anymore-well-almost-2b1faac3508 Then got the error after Run button clicked: Error:Execution failed for task ':app:shrinkDebugMultiDexComponents'. > java.io.IOException: Can't read [/Volumes/Disk2/Users/fabin/Documents/workspace-git/WechatSDK-Demo/app/build/intermediates/multi-dex/debug/allclasses.jar] (Can't process class [com/tencent/wxop/stat/al.class] (Unknown verification type [95] in stack map frame))

--- app/src/main/java/com/corising/demo/wechat/DemoApplication.java
+++ app/src/main/java/com/corising/demo/wechat/DemoApplication.java
@@ -1,10 +1,12 @@
 package com.corising.demo.wechat;
 
-import android.app.Application;
+import android.support.multidex.MultiDexApplication;
 
 /**
- * Application
+ * Application that extends MultiDexApplication to avoid DEX’s 64k limit.
+ * <p/>
+ * See section <i>Using the official solution</i> in <a href="https://medium.com/@mustafa01ali/dexs-64k-limit-is-not-a-problem-anymore-well-almost-2b1faac3508">https://medium.com/@mustafa01ali/dexs-64k-limit-is-not-a-problem-anymore-well-almost-2b1faac3508</a>
  * Created by fabin on 12/17/14.
  */
-public class DemoApplication extends Application {
+public class DemoApplication extends MultiDexApplication {
 }
