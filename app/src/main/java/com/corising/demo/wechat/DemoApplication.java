246a1273e6266c171a18f7871938c5127a089e17
闫发宾 <fabin@yanfabins-MacBook-Pro.local>
2014-12-17 17:39:58 +0800

    Add a blank DemoApplication and it works fine.

---
+++ app/src/main/java/com/corising/demo/wechat/DemoApplication.java
@@ -0,0 +1,10 @@
+package com.corising.demo.wechat;
+
+import android.app.Application;
+
+/**
+ * Application
+ * Created by fabin on 12/17/14.
+ */
+public class DemoApplication extends Application {
+}
